2b8ce6f1872c3392d28bbea01778b79b39f409f6
Ana Moreno <anamariamoreno@gmail.com>
2019-02-04 10:44:23 +0100

    first update

--- src/lib/auth-service.js
+++ src/lib/auth-service.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 class Auth {
   constructor() {
     this.auth = axios.create({
-      baseURL: 'http://localhost:5000',
+      baseURL: process.env.REACT_APP_API_URL,
+      // baseURL: 'http://localhost:5000',
       withCredentials: true
     })
   }
